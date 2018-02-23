5d84b73c2947c402c2ad72f3359258e92cebe2e2
Rachel Harvey <raharvey@bsu.edu>
2018-02-23 14:34:17 -0500

    create UsernameSearchViewController

---
+++ GitHub Followers/GitHub Followers/UsernameSearchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  UsernameSearchViewController.swift
+//  GitHub Followers
+//
+//  Created by Rachel Harvey on 2/23/18.
+//  Copyright © 2018 Rachel Harvey. All rights reserved.
+//
+
+import UIKit
+
+class UsernameSearchViewController: UIViewController {
+    
+}
