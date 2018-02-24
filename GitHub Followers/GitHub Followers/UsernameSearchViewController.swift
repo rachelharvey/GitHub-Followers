79cdb1e0c0e9ae099945ca1efc40b369501947b7
Rachel Harvey <raharvey@bsu.edu>
2018-02-23 19:26:10 -0500

    add status label

--- GitHub Followers/GitHub Followers/UsernameSearchViewController.swift
+++ GitHub Followers/GitHub Followers/UsernameSearchViewController.swift
@@ -12,8 +12,31 @@ class UsernameSearchViewController: UIViewController,UITextFieldDelegate,Network
     
     var usernameTextField: UITextField!
     
+    @IBOutlet weak var statusLabel: UILabel?
+    
+    private var _status: String!
+    var status: String! {
+        get {
+            return self._status
+        }
+        set {
+            print(newValue)
+            self._status = newValue
+            DispatchQueue.main.async {
+                self.statusLabel?.text = self.status
+            }
+        }
+    }
+    
+    let defaultText = "Type a GitHub username in the search bar to see that user's followers!"
+    let connectingText = "Please wait a moment while we look for the user's followers..."
+    let successText = "Huzzah! Found user's followers!"
+    let requestErrorText = "Uh-oh! There was a problem getting that user's followers, make sure you are connected to the internet and the username is spelled correctly and try again."
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.statusLabel?.text = self.defaultText
         self.addSearchBarButtonItems()
         NetworkRequester.connection.delegate = self
     }
@@ -52,6 +75,7 @@ class UsernameSearchViewController: UIViewController,UITextFieldDelegate,Network
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if let username = self.usernameTextField.text {
+            self.statusLabel?.text = self.connectingText
             NetworkRequester.connection.getFollowers(username: username)
         }
         textField.resignFirstResponder()
@@ -59,6 +83,7 @@ class UsernameSearchViewController: UIViewController,UITextFieldDelegate,Network
     }
     
     @objc func cancelSearch() {
+        self.statusLabel?.text = self.defaultText
         self.usernameTextField.resignFirstResponder()
         self.usernameTextField.text = ""
     }
@@ -66,10 +91,13 @@ class UsernameSearchViewController: UIViewController,UITextFieldDelegate,Network
     //----------NetworkRequesterDelegate----------
     
     func followersRecieved(array: NSArray) {
+        self.status = self.successText
         print(array)
     }
     
     func requestError() {
+        self.status = self.requestErrorText
+//        self.statusLabel?.text = self.requestErrorText
         print("Request Error")
     }
     
