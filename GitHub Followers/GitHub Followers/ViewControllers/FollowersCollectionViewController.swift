d414d18072f517f19caf3b5e6722033cbf8c470e
Rachel Harvey <raharvey@bsu.edu>
2018-02-23 19:37:55 -0500

    add FollowersCollectionViewController

---
+++ GitHub Followers/GitHub Followers/ViewControllers/FollowersCollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FollowersCollectionViewController.swift
+//  GitHub Followers
+//
+//  Created by Rachel Harvey on 2/23/18.
+//  Copyright © 2018 Rachel Harvey. All rights reserved.
+//
+
+import UIKit
+
+class FollowersCollectionViewController: UICollectionViewController {
+    
+}
