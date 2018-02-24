f45b96182beaeb78b8565731a9f1568aeed559ed
Rachel Harvey <raharvey@bsu.edu>
2018-02-23 21:48:15 -0500

    create and attach singleFollowerViewController

---
+++ GitHub Followers/GitHub Followers/View Controllers/SingleFollowerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SingleFollowerViewController.swift
+//  GitHub Followers
+//
+//  Created by Rachel Harvey on 2/23/18.
+//  Copyright © 2018 Rachel Harvey. All rights reserved.
+//
+
+import UIKit
+
+class SingleFollowerViewController: UIViewController {
+    var follower: NSDictionary!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(self.follower)
+    }
+}
